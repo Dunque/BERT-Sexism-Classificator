1bcd2543d83124adf77ad784a2f8abafa6ad0f11
Roi Santos <roi.santos.rios@gmail.com>
2022-06-17 13:22:30 +0200

    bertweet metrics

--- binEngBERTweet.py
+++ binEngBERTweet.py
@@ -33,7 +33,9 @@ from sklearn.metrics import classification_report, confusion_matrix
 ##Whole dataset training evaluation (softmax)
 import torch.nn.functional as F
 
-from TweetNormalizer import normalizeTweet
+## Tweet normalizer (imported from bertweet's github)
+from emoji import demojize
+from nltk.tokenize import TweetTokenizer
 
 # Data paths
 translated_data = 'data/EXIST2021_translatedTraining.csv'
@@ -81,6 +83,51 @@ else:
     print('No GPU available, using the CPU instead.')
     device = torch.device("cpu")
 
+
+def normalizeToken(token):
+    lowercased_token = token.lower()
+    if token.startswith("@"):
+        return "@USER"
+    elif lowercased_token.startswith("http") or lowercased_token.startswith("www"):
+        return "HTTPURL"
+    elif len(token) == 1:
+        return demojize(token)
+    else:
+        if token == "’":
+            return "'"
+        elif token == "…":
+            return "..."
+        else:
+            return token
+
+
+def normalizeTweet(tweet):
+    tokens = TweetTokenizer().tokenize(tweet.replace("’", "'").replace("…", "..."))
+    normTweet = " ".join([normalizeToken(token) for token in tokens])
+
+    normTweet = (
+        normTweet.replace("cannot ", "can not ")
+        .replace("n't ", " n't ")
+        .replace("n 't ", " n't ")
+        .replace("ca n't", "can't")
+        .replace("ai n't", "ain't")
+    )
+    normTweet = (
+        normTweet.replace("'m ", " 'm ")
+        .replace("'re ", " 're ")
+        .replace("'s ", " 's ")
+        .replace("'ll ", " 'll ")
+        .replace("'d ", " 'd ")
+        .replace("'ve ", " 've ")
+    )
+    normTweet = (
+        normTweet.replace(" p . m .", "  p.m.")
+        .replace(" p . m ", " p.m ")
+        .replace(" a . m .", " a.m.")
+        .replace(" a . m ", " a.m ")
+    )
+
+    return " ".join(normTweet.split())
 
 # BERT
 
